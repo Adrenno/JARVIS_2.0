67878196be95952f94f6a4338857603c6c6b84ae
Adrenno <62647788+Adrenno@users.noreply.github.com>
2023-02-16 15:35:17 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit=async(e) => {
   loader(messageDiv);
 
   //fetch data from server (get bot response)
-  const response=await fetch('http://localhost:5000', {
+  const response=await fetch('https://j-a-r-v-i-s-2-0.onrender.com', {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({
